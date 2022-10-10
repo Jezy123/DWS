112d3c7d80cde341fc482b41303ed55af99d8836
Jezy123 <juanmapg321@gmail.com>
2022-10-10 13:50:55 +0200

    Progreso symfony

--- symfony-contactos/src/Controller/ContactoController.php
+++ symfony-contactos/src/Controller/ContactoController.php
@@ -1,6 +1,8 @@
 <?php
 namespace App\Controller;
 
+use App\Entity\Contacto;
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -23,15 +25,36 @@ class ContactoController extends AbstractController
 
     ];     
     
+    #[Route('/contacto/insertar', name: 'ficha_contacto')]
+    public function insertar(ManagerRegistry $doctrine)
+    {
+        $entityManager= $doctrine->getManager();
+        foreach($this->contactos as $c){
+            $contacto =new Contacto();
+            $contacto->setNombre($c["nombre"]);
+            $contacto->setTelefono($c["telefono"]);
+            $contacto->setEmail($c["email"]);
+            $entityManager->persist($contacto);
+        }
+        try{
+            $entityManager->flush();
+            return new Response("Contactos insertados");
+        } catch (\Exception $e){
+            return new Response("Error insertando");
+        }
+
+
+    }
+
     #[Route('/contacto/{codigo}', name: 'ficha_contacto')]
 
-    public function ficha($codigo):Response{
-        $resultado = ($this->contactos[$codigo] ?? null);
+    public function ficha(ManagerRegistry $doctrine , $codigo):Response{
+        $repositorio = $doctrine->getRepository(Contacto::class);
+        $contacto =$repositorio->find($codigo);
 
-        $resultado =($this->contactos[$codigo]?? null);
-            return $this->render ('ficha_contacto.html.twig',[
-            'contacto' => $resultado
-            ]);
+        return $this->render('ficha_contacto.html.twig',[
+            'contacto' => $contacto
+        ]);
       
     }
 
@@ -44,22 +67,9 @@ class ContactoController extends AbstractController
             }
         );
 
-    if (count($resultados)){
-        $html = "<ul>";
-        foreach($resultados as $id=> $resultados){
-            $html .= "<li>" . $id ."</li>";
-                $html .= "<li>" . $resultados['nombre'] ."</li>";
-                $html .= "<li>" . $resultados['telefono'] ."</li>";
-                $html .= "<li>" . $resultados['email'] ."</li>";
-        }
-        $html .= "<ul>";
-
-            return new Response("<html><body>$html</body>");
-        }else{
-            return new Response("<html><body>No se ha encontrado ningun contacto</body>");
-        }
+        return $this->render('lista_contacto.html.twig',[
+            'contactos' => $resultados
+        ]);
     }
-    
-
     
 }
