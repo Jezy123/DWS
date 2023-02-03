8f30c904490dfa66245cf5321be536032a07ac4d
Jezy123 <juanmapg321@gmail.com>
2023-02-03 13:32:21 +0100

    whatsapp Candela Juanma

--- symfony-whatsapp/public/js/main.js
+++ symfony-whatsapp/public/js/main.js
@@ -1,15 +1,17 @@
 $(document).ready(function(){
+
+    
 var toUserId=""
     $("#message_form_text").keypress(function(event){
         if (event.which==13){
-        
+ 
             $.post( `/mensaje/enviar/${toUserId}`, $(this).serialize())
             $(this).val("")
             
         }
     })
 
-    $(".contact").on("click", function (){
+    $(".contact").on("click", function cargar(){
         $("#messages").html("")
         var idUsuario=($('#user').data("user-id"))
         toUserId = $(this).data("id")
@@ -54,5 +56,72 @@ var toUserId=""
 
     })
 
+	//Open a WebSocket connection.
+	websocket = new WebSocket("ws://localhost:9000/");
+
+    //Connected to server
+	websocket.onopen = function(ev) {
+		console.log('Connected to server ');
+        //prepare json data
+        var msg = {
+            type: 'chatData',
+            fromUserId: ($('#user').data("user-id")),
+            toUserId:'',
+        };
+        //convert and send data to server
+        websocket.send(JSON.stringify(msg));
+	}
+
+    //Connection close
+	websocket.onclose = function(ev) {
+    	console.log('Disconnected');
+    };
+    websocket.onmessage = function(evt) {
+        var response = JSON.parse(evt.data); //PHP sends Json data
+        //hacer lo que corresponda con response
+        console.log(response.message.text)
+        var mensajeOtro =  `<div class="flex mb-2">
+        <div class="rounded py-2 px-3" style="background-color: #F2F2F2">
+            <p class="text-sm text-teal">
+            ${response.message.from_user_name}
+            </p>
+            <p class="text-sm mt-1">
+            ${response.message.text}
+            </p>
+            <p class="text-right text-xs text-grey-dark mt-1">
+            ${response.message.timestamp.date}
+            </p>
+         </div>
+        </div>`
+                
+        var mensajeMio= `<div class=" misMensajes flex justify-end mb-2">
+                <div class="rounded py-2 px-3" style="background-color: #E2F7CB">
+                    <p class="text-sm mt-1">
+                    ${response.message.text}
+                    </p>
+                    <p class="text-right text-xs text-grey-dark mt-1">
+                    ${response.message.timestamp.date}
+                    </p>
+                </div>
+            </div>`
+
+        
+        if(response.message.type=="chatmsg"){
+        if(response.fromUserId==($('#user').data("user-id"))){
+         $('#messages').append(mensajeOtro)
+    
+        }else{
+            $('#messages').append(mensajeMio)
+        }
+        }
+    }
+
+    //Error
+	websocket.onerror = function(ev) {
+    	console.log('Error '+ev.data);
+    };
+
+
 })
+
 
